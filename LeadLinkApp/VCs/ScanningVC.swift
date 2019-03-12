2b050e619d0cc019a87cf0082288e8b03b843cd6
markodimitrije <markodimitrije@gmail.com>
2019-03-12 12:44:27 +0100

    save state, iPad layout for scanning screen

---
+++ LeadLinkApp/VCs/ScanningVC.swift
@@ -0,0 +1,19 @@
+//
+//  ScanningVC.swift
+//  LeadLinkApp
+//
+//  Created by Marko Dimitrijevic on 12/03/2019.
+//  Copyright © 2019 Marko Dimitrijevic. All rights reserved.
+//
+
+import UIKit
+
+class ScanningVC: UIViewController {
+
+    var campaignId: Int? // ako ti neko dobaci ovo, mozes da izvuces campaign (Logo, questions, answers....) (iz dataProvider-a)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
