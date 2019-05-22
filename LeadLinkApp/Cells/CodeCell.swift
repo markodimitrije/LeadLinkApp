70ed7ab2eeeb78b44192abcf3e77ff0371851cff
Dimitrijevic Marko <markodimitrije@gmail.com>
2019-05-22 17:12:45 +0200

    save state before applying reports screen instead of codes screen

---
+++ LeadLinkApp/Cells/CodeCell.swift
@@ -0,0 +1,9 @@
+//
+//  CodeCell.swift
+//  LeadLinkApp
+//
+//  Created by Marko Dimitrijevic on 22/05/2019.
+//  Copyright © 2019 Marko Dimitrijevic. All rights reserved.
+//
+
+import Foundation
---
+++ LeadLinkApp/Model/CodeInfo.swift
@@ -0,0 +1,9 @@
+//
+//  CodeInfo.swift
+//  LeadLinkApp
+//
+//  Created by Marko Dimitrijevic on 22/05/2019.
+//  Copyright © 2019 Marko Dimitrijevic. All rights reserved.
+//
+
+import Foundation
