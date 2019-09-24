676e9af4005ac029592b244258cbfd6dae1e418d
Dimitrijevic Marko <markodimitrije@gmail.com>
2019-09-24 13:38:39 +0200

    headline label text system 16.0 bold, but not resizable due to text content

---
+++ LeadLinkApp/Views/QAViews/HeadlineLabelView/HeadlineLabelView.swift
@@ -0,0 +1,9 @@
+//
+//  HeadlineLabelView.swift
+//  LeadLinkApp
+//
+//  Created by Marko Dimitrijevic on 24/09/2019.
+//  Copyright © 2019 Marko Dimitrijevic. All rights reserved.
+//
+
+import Foundation
