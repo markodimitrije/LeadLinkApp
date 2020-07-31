d07a78dcfe9a917ddf3c9205b31439bd6eb8634e
Dimitrijevic Marko <markodimitrije@gmail.com>
2020-07-31 15:38:23 +0200

    no prepopulated data, no deletedAt field on campaigns (campaignsVersionChecker exists)

--- LeadLinkApp/Application/AppDelegate.swift
+++ LeadLinkApp/Application/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
 
-        let dataBaseMigrator: DataBaseMigrating = RealmSchemaMigrator.init(newVersion: 12)
+        let dataBaseMigrator: DataBaseMigrating = RealmSchemaMigrator.init(newVersion: 1)
         dataBaseMigrator.migrate()
 
         return true
